1d5d09e8e6697c555a8f00c4a24d4fd18bc67488
Marcinszymannnski <marcin.szymanski195@gmail.com>
2025-06-01 14:18:23 +0200

    drobne poprawki w docker-compose, połaczenie orders z payments i zmiany z tym związane

--- orders/app.py
+++ orders/app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Order service is running"
+
+@app.route("/check-payment")
+def check_payment():
+    try:
+        response = requests.get("http://payments:5000/")
+        return f"Payment service response: {response.text}"
+    except Exception as e:
+        return f"Error contacting payments: {str(e)}"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
